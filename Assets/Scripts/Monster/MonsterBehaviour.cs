326524746dd27985219a29ae7af511589d6cab57
Raidenkyu <joao-carlos.14@hotmail.com>
2020-04-11 01:51:20 +0100

    Added enemy vision

--- Assets/Scripts/Monster/MonsterBehaviour.cs
+++ Assets/Scripts/Monster/MonsterBehaviour.cs
@@ -6,6 +6,7 @@ public class MonsterBehaviour : MonoBehaviour
 {
     public enum MonsterState {ROAM, PREY, ATTACK, FREEZE };
     private MonsterState state;
+    public float visionLength = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,29 @@ public class MonsterBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+                // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        Vector3 src = transform.position;
+        Vector3 dest = transform.TransformDirection(Vector3.forward);
+
+        src.y = 1;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(src, dest, out hit, visionLength, layerMask))
+        {
+            Debug.DrawRay(src, dest * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(src, dest * visionLength, Color.white);
+        }
     }
 
     MonsterState GetState() {
