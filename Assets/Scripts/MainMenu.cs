7823c9cfefa700292626301240ec88e00cffb0b3
JoaoAlvaroFerreira <joaoalvaro.ferreira@hotmail.com>
2020-05-01 03:26:46 +0100

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public void Level0() {
+        SceneManager.LoadScene("Level0");
+        Time.timeScale = 1;
+    }
+
+    public void Level1() {
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1;
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
