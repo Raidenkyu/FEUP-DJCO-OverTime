2e1d64a9eba99d9ac46c75edeb32bdda5631194a
Raidenkyu <joao-carlos.14@hotmail.com>
2020-05-13 14:53:37 +0100

    Fixed ray angle

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -11,8 +11,9 @@ public class Gun : MonoBehaviour
     public float totalShifts = 2;
     public float currentShifts;
 
-    
     public TextMeshPro tmp;
+
+    public GameObject firePoint;
 
     // Update is called once per frame
     void Start(){
@@ -49,8 +50,8 @@ public class Gun : MonoBehaviour
         // Bit shift the index of the layer (8) to get a bit mask
         int layerMask = 1 << 13;
         
-        Vector3 src = gun.transform.position;
-        Vector3 dest = gun.transform.forward;
+        Vector3 src = firePoint.transform.position;
+        Vector3 dest = firePoint.transform.forward;
 
         RaycastHit Hit;        
         if (Physics.Raycast(src, dest, out Hit, range, layerMask)) {
