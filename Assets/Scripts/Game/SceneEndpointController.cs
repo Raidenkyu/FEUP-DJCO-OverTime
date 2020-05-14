2f3cb561ed9c112acd9ee5f653d5457fcad69ec7
Fernando Alves <up201605270@fe.up.pt>
2020-05-15 00:47:21 +0100

    Added SceneEndpointController script

---
+++ Assets/Scripts/Game/SceneEndpointController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SceneEndpointController : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.CompareTag("Player")) {
+            SceneController.Instance.LevelComplete();
+        }
+
+    }
+
+}
