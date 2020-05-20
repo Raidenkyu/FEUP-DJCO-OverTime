dabce5f368f888d0f33b07d4e86d9cd0537f10c9
Raidenkyu <joao-carlos.14@hotmail.com>
2020-05-20 01:27:56 +0100

    Added colors to doors

---
+++ Assets/Scripts/Decoration/DoorColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorColor : MonoBehaviour
+{
+    public Button button;
+    public MeshRenderer mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh.materials[0].color = button.buttonColor;
+    }
+}
