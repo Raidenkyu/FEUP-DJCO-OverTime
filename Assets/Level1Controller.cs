4a6f8630787cdcf31530b305ffd38a09c81f3d80
Raidenkyu <joao-carlos.14@hotmail.com>
2020-05-31 17:54:20 +0100

    Added Pickable Gun logic

---
+++ Assets/Level1Controller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Controller : MonoBehaviour
+{
+    public void PickGun() {
+
+    }
+}
---
+++ Assets/Scripts/Decoration/PickableGun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableGun : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void Interact() {
+        Activated.Invoke();
+        Destroy(gameObject);
+    }
+}
