a909494041aebe6cd6b6dd5aa2f9352ed7cdfdc0
Raidenkyu <joao-carlos.14@hotmail.com>
2020-06-05 18:21:17 +0100

    Added door to level 1

---
+++ Assets/Scripts/Decoration/DoorController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+public class DoorController : MonoBehaviour {
+    public StudioEventEmitter soundEvent;
+
+    void PlaySound() {
+        soundEvent.Play();
+    }
+
+    public void Open() {
+        gameObject.SetActive(false);
+        Invoke("PlaySound", 0.1f);
+    }
+
+    public void Close() {
+        gameObject.SetActive(true);
+        Invoke("PlaySound", 0.1f);
+    }
+}
--- Assets/Scripts/Levels/Level1Controller.cs
+++ Assets/Scripts/Levels/Level1Controller.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Level1Controller : MonoBehaviour
 {
+    public DoorController door;
+
     public void PickGun() {
         SceneController.Instance.GetMainPlayerMovement().TogglePlayerGun(true);
+        door.Open();
     }
 }
