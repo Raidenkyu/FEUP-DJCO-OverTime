aed8cf1efc7264e5d9783f7fa4d7d5598df5905f
Raidenkyu <joao-carlos.14@hotmail.com>
2020-06-06 19:07:34 +0100

    Added earthquake

--- Assets/Scripts/Levels/Level0Controller.cs
+++ Assets/Scripts/Levels/Level0Controller.cs
@@ -4,16 +4,7 @@ using FMODUnity;
 public class Level0Controller : MonoBehaviour {
     public GameObject Object;
 
-    public StudioEventEmitter earthquakeEvent;
     public StudioEventEmitter furnitureEvent;
-
-    void Start() {
-        earthquakeEvent.Play();
-    }
-
-    void PlayEarthQuake() {
-        earthquakeEvent.Play();
-    }
 
     public void MoveObject() {
         Object.transform.position = new Vector3(Object.transform.position.x, Object.transform.position.y, Object.transform.position.z + 3);
