ad2b77bdc1c15fddb4d378488aab73d798c958ba
Raidenkyu <joao-carlos.14@hotmail.com>
2020-06-08 17:39:29 +0100

    Added chained dialogs

---
+++ Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using FMODUnity;
+
+public class Dialog : MonoBehaviour {
+    public float duration;
+    public StudioEventEmitter dialogEvent;
+    public GameObject dialogText;
+    public Dialog chainedDialog;
+    public float chainDelay = 0;
+
+    IEnumerator ShowDialog() {
+        dialogEvent.Play();
+        dialogText.SetActive(true);
+
+        yield return new WaitForSeconds(duration);
+
+        if (chainedDialog != null) {
+            if (chainDelay != 0) yield return new WaitForSeconds(chainDelay);
+
+            chainedDialog.StartDialog();
+        }
+
+        Destroy(gameObject);
+    }
+
+    public void StartDialog() {
+        StartCoroutine(ShowDialog());
+    }
+}
---
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using FMODUnity;
+
+public class DialogTrigger : Dialog {
+    public Collider triggerCollider;
+
+    private void OnTriggerEnter(Collider collider) {
+        triggerCollider.enabled = false;
+        StartDialog();
+    }
+}
