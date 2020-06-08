811585ec0906ef019b93ad37dc2e259e2f685eef
tripor <figorifico3000@gmail.com>
2020-06-08 22:57:52 +0100

    Shinny pressure plates

--- Assets/Scripts/Decoration/lightColour.cs
+++ Assets/Scripts/Decoration/lightColour.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class lightColour : MonoBehaviour
 {
     public Button button;
+    public PressurePlate plate;
     public MeshRenderer cilinder_mesh;
     public new Light light;
     // Start is called before the first frame update
     void Start()
     {
-        cilinder_mesh.materials[0].color = button.buttonColor;
-        cilinder_mesh.materials[0].SetColor("_EmissionColor", button.buttonColor);
-        light.color = button.buttonColor;
+        if(button)
+        {
+            cilinder_mesh.materials[0].color = button.buttonColor;
+            cilinder_mesh.materials[0].SetColor("_EmissionColor", button.buttonColor);
+            light.color = button.buttonColor;
+        }
+        else if(plate)
+        {
+            cilinder_mesh.materials[0].color = plate.plateColor;
+            cilinder_mesh.materials[0].SetColor("_EmissionColor", plate.plateColor);
+            light.color = plate.plateColor;
+        }
     }
 
 }
