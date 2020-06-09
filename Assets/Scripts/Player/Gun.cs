c6fccd41d830c4418ddd4e897dbaa491d43e5481
Raidenkyu <joao-carlos.14@hotmail.com>
2020-06-09 14:33:50 +0100

    Add Monster Freeze obstacles

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -26,43 +26,24 @@ public class Gun : MonoBehaviour {
     // Update is called once per frame
     void Start() {
         currentShifts = totalShifts;
-
-    }
-    void Update() {
-
     }
 
     private void FixedUpdate() {
-
         clockTicks();
-
-
         updateParticle();
-
-
     }
 
     public void clockTicks() {
         if (Time.time >= nextTime) {
-
-
             tmp.text = currentTime.ToString();
             nextTime += interval;
-
         }
-
-    }
-
-    public void lightUpWires() {
-
     }
 
     public void updateParticle() {
-
         if (fireParticle.activeSelf) {
             fireParticle.transform.position += firePoint.transform.forward / 2;
             Invoke("ResetParticle", 2.0f);
-
         }
     }
 
@@ -75,27 +56,26 @@ public class Gun : MonoBehaviour {
         soundEvent.Play();
         fireParticle.SetActive(true);
         FreezeMonster();
-        ResetTime();
     }
 
     void FreezeMonster() {
         // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 13;
+        int layerMask = LayerMask.GetMask("Monster", "Default", "Environment");
 
         Vector3 src = firePoint.transform.position;
         Vector3 dest = firePoint.transform.forward;
 
         RaycastHit Hit;
         if (Physics.Raycast(src, dest, out Hit, range, layerMask)) {
+            GameObject obj = Hit.collider.gameObject;
             Debug.DrawRay(src, dest * Hit.distance, Color.white);
-            Debug.Log(Hit.collider.tag);
-            Hit.collider.gameObject.GetComponent<MonsterBehaviour>()?.Freeze();
+            Debug.Log(obj.tag);
+
+            if (!obj.CompareTag("Monster")) return;
+            
+            obj.GetComponent<MonsterBehaviour>()?.Freeze();
         } else {
             Debug.DrawRay(src, dest * range, Color.white);
         }
-    }
-
-    void ResetTime() {
-
     }
 }
