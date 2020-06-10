28386861dc2b4177cdcf270bd2e55735414bd95c
Fernando Alves <up201605270@fe.up.pt>
2020-06-10 16:51:53 +0100

    Added Killing Surface prefab

---
+++ Assets/Scripts/Game/KillingSurface.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class KillingSurface : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("Kill");
+            if (!other.GetComponent<PlayerMovement>().GetIsGhost()) {
+                SceneController.Instance.PlayerDied();
+            }
+        }
+    }
+}
