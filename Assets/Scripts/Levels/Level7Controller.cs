61eb45b08d0c5f6993caba1fee2f343695f52740
Fernando Alves <up201605270@fe.up.pt>
2020-06-17 23:13:14 +0100

    Added logic for final level dialog

---
+++ Assets/Scripts/Levels/Level7Controller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Level7Controller : MonoBehaviour {
+    public GameObject startingDialog;
+    public bool canSkipToMenu = false;
+
+    void Start() {
+        SceneController.Instance.GetMainPlayerMovement().controller.enabled = false;
+        Invoke("StartVoiceLines", 2.0f);
+        Invoke("AllowSkipToMenu", 5.0f); // TODO: Adjust this value to match the sum of subtitles
+    }
+
+    void StartVoiceLines () {
+        startingDialog.GetComponent<Dialog>().StartDialog();
+    }
+
+    void AllowSkipToMenu () {
+        canSkipToMenu = true;
+    }
+
+    void Update() {
+        if (canSkipToMenu && Input.anyKeyDown) {
+            SceneController.Instance.LevelComplete();
+        }
+    }
+}
